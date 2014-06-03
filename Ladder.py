8cdbdaec9ed00abc1e8d39192debb2922f2a14cb
kikar0 <dovfields@gmail.com>
2014-06-03 09:59:26 -0400

    Update Ladder.py
    Adding and implementing hiatus. If I get a chance, I will try to also implement defenses and auto-priority rules. Finally, I want a way to print the weekly challenges. Note: I haven't debugged this (sorry)

--- Ladder.py
+++ Ladder.py
@@ -6,11 +6,13 @@ from glicko2 import Rating, Glicko2
 
 
 class member:
-    def __init__(self, name, position=0, defences=0, lastplayed="",mu=0,sigma=0,volativity=0):
+    def __init__(self, name, position=0, defences=0, lastplayed="",mu=0,sigma=0,volativity=0, hiatus = False):
         self.name = name
         self.position = int(position)
         self.defences = int(defences)
         self.lastplayed = lastplayed
+        self.hiatus = hiatus
+        self.inChallenge = False
         if not (mu or sigma or volativity):
             self.rating = Rating()
         else:
@@ -18,13 +20,33 @@ class member:
 
     def writeData(self):
         #Username,Position,Defences,Lastplayed,rating_mu,rating_sigma,rating_volativity
-        return self.name,str(self.position),str(self.defences),self.lastplayed,self.rating.mu,self.rating.sigma,self.rating.volatility
+        return self.name,str(self.position),str(self.defences),self.lastplayed,self.rating.mu,self.rating.sigma,self.rating.volatility, self.hiatus
 
     def writeOutput(self=None):
         if not self:
-            return "Position","Username","Rating"#,"+Defences/Challenges-"
-        return str(self.position),self.name,format(self.rating.mu,"1.0f")#,str(self.defences) if int(self.defences)<0 else "+"+str(self.defences)
-
+            return "Position","Username","Defences","Rating", "Hiatus"#,"+Defences/Challenges-"
+        onHiatus = ""
+        if self.hiatus:
+            onHiatus = "On Hiatus"
+        return str(self.position),self.name,str(self.defences),format(self.rating.mu,"1.0f"),onHiatus#,str(self.defences) if int(self.defences)<0 else "+"+str(self.defences)
+    def setHiatus(self, hiatusStatus)
+        #set hiatus status to true or false
+        self.hiatus = hiatusStatus
+    def getHiatus(self)
+        #get hiatus status
+        return self.hiatus
+    def setChallenge(self,challengeStatus) 
+        #set challenge status to true or false
+        self.inChallenge = challengeStatus
+    def getChallenge
+        #get challenge status
+        return self.inChallenge
+    def addDefence(self)
+        self.defences += 1
+    def resetDefence(self)
+        self.defences = 0
+    def getDefence(self)
+        return self.defences
 class challenge:
     def __init__(self, member1, member2, date):
       self.member1 = member1
@@ -102,13 +124,20 @@ class ladder(object):
             return print("Can't challenge - "+member1+" not in ladder")
         elif not member2 in self.members:
             return print("Can't challenge - "+member2+" not in ladder")
+        #Check hiatus status
+        elif self.members.get(member1).getHiatus():
+            return print("Can't challenge - "+member1+" is on hiatus")
+        elif self.members.get(member2).getHiatus():
+            return print("Can't challenge - "+member2+" is on hiatus")
         #Check if the parties are less than x spaces away.
         #TODO: Remove magic number
-        elif abs(self.members.get(member1).position - self.members.get(member2).position) > 3:
+        elif getDistance(member1,member2) > 3:
             return print("Can't challenge - "+member1+" & "+member2+" are too separated")
+        
         else:
+            #This seems to be an unnecessary step -> people can be challenged multiple times. For right now, I'm going to take it out
             #Check if either party is already in a challenge.
-            for chall in self.challenges:
+            """for chall in self.challenges:
                 if chall.isInChallenge(member1):
                     #Member 1 is already in a challenge
                     #Assume they's the challenger and can cancel and re challenge?
@@ -116,7 +145,7 @@ class ladder(object):
                 if chall.isInChallenge(member2):
                     #Member 2 is already in a challenge
                     #Apply challenge rules
-                    print(member2+" in challenge")
+                    print(member2+" in challenge")"""
 
                 #If not add new challenge
                 self.challenges.append(challenge(self.members.get(member1),self.members.get(member2), date))
@@ -143,7 +172,22 @@ class ladder(object):
             mem_user.rating,mem_versus.rating = glick.rate_1vs1(mem_user.rating,mem_versus.rating)
         else:
             mem_versus.rating,mem_user.rating = glick.rate_1vs1(mem_versus.rating,mem_user.rating)
-    
+    #Calculate Distance given hiatus status for challenging
+    def getDistance(self, member1, member2);
+        positionList = sorted(self.members.items(), key=lambda mem: mem[1].position)
+        distance = 0
+        mem1pos = self.members.get(member1).position
+        mem2pos = self.members.get(member2).position
+        if mem1pos > mem2pos:
+            startPosition = mem2pos
+            endPosition = mem1pos
+        else:
+            startPosition = mem2pos
+            endPosition = member2.position
+        for mem in positionList[startPosition:endPosition]:
+            if not mem.getHiatus:
+                distance += 1
+        return distance-1
     def __str__(self):
         x = PrettyTable(member.writeOutput())
         for mem in sorted(self.members.items(), key=lambda mem: mem[1].position):
